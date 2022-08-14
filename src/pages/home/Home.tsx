fc6d42cb04923f747764439ca80a8476f06a0ed7
mvogee <mvogee@yahoo.com>
2022-08-13 19:33:02 -0700

    home page content

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,16 @@ const Home = () => {
     return(
         <div className="home">
             <Nav />
-            <h1>Home</h1>
+            <div className="Welcome">
+                <h1>Welcome to Sparky Aid</h1>
+                <h2>An Electricians best friend.</h2>
+                <p>The electrician already has a hard job. Sparky aids goal 
+                    is to make that job just a little bit easier by freeing up your 
+                    brain power to solving the problems that take creativity. 
+                    Pipe bending calculations, wire sizing, bla bla. We got you 
+                    covered. So get back to doing work and let us do the 
+                    calculating! Be safe out there.</p>
+            </div>
         </div>
     );
 }
