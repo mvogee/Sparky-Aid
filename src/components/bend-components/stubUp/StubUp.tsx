38d9a7b2910540855c3ae75bc39c25490d34b2b1
mvogee <mvogee@yahoo.com>
2022-09-07 20:40:59 -0700

    created utils.js getTakeup utility function. stubUp working correctly. BackToBackStubUp bend calculations working properly.

--- src/components/bend-components/stubUp/StubUp.tsx
+++ src/components/bend-components/stubUp/StubUp.tsx
@@ -1,33 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import MeasurementInput from "../measurementInput/MeasurementInput"
+import { getTakeup } from "../../../utils";
 
 type AppProps = {
     conduitSize: string;
     conduitType: string;
 }
 
-const getTakeup = (size: string, type: string) => {
-    let takeup = 0;
-    switch (size) {
-        case "1/2":
-            takeup = (type === "EMT" ? 5 : 6);
-            break;
-        case "3/4":
-            takeup = (type === "EMT" ? 6 : 8);
-            break;
-        case "1":
-            takeup = (type === "EMT" ? 8 : 11);
-            break;
-        case "1 1/4":
-            takeup = (type === "EMT" ? 11 : 88);
-            break;
-        default:
-            takeup = 0;
-            break;
-    }
-    return (takeup);
-}
+
 
 const StubUp = (props: AppProps) => {
 const [distanceToBend, setDistanceToBend] = useState<number>(0);
