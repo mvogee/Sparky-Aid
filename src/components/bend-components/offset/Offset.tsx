05f64fa0a3035e9c1104ffc73c3bd4c1bfb36275
mvogee <mvogee@yahoo.com>
2022-09-14 20:52:57 -0700

    offset calculations displayed

--- src/components/bend-components/offset/Offset.tsx
+++ src/components/bend-components/offset/Offset.tsx
@@ -13,6 +13,12 @@ type AppProps = {
 const Offset = (props: AppProps) => {
 
     const [offsetHeight, setOffsetHeight] = useState<number>(0);
+    //const [degreeBends, setDegreeBends] = useState<string>("10deg");
+    const [multiplier, setMultiplier] = useState<number>(6);
+
+    const changeDegree = (event: any) => {
+        setMultiplier(parseFloat(event.target.value));
+    };
 
     return (
         <div className="offset-wrapper">
@@ -21,13 +27,17 @@ const Offset = (props: AppProps) => {
                 value={offsetHeight}
                 updateValueFunction={setOffsetHeight}
             />
-            <select >
-                <option>10deg</option>
-                <option>22.5deg</option>
-                <option>30deg</option>
-                <option>45deg</option>
-                <option>60deg</option>
+            <select onChange={changeDegree}>
+                <option value="6">10deg</option>
+                <option value="2.6">22.5deg</option>
+                <option value="2">30deg</option>
+                <option value="1.4">45deg</option>
+                <option value="1.2">60deg</option>
             </select>
+            <div className="display">
+                <p>first bend: at least {offsetHeight * multiplier}" from obstacle.</p>
+                <p>second bend: mark {offsetHeight * multiplier}" from first bend</p>
+            </div>
         </div>
     );
 }
